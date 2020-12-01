f15753ccc6988ed0aabdde2102ef2d637f3e33d9
llw <1317057860@qq.com>
2020-12-01 10:05:36 +0800

    增加高精度文字识别

--- app/src/main/java/com/llw/textocr/MainActivity.java
+++ app/src/main/java/com/llw/textocr/MainActivity.java
@@ -1,6 +1,7 @@
 package com.llw.textocr;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -17,6 +18,7 @@ import com.baidu.ocr.sdk.OnResultListener;
 import com.baidu.ocr.sdk.exception.OCRError;
 import com.baidu.ocr.sdk.model.AccessToken;
 import com.baidu.ocr.ui.camera.CameraActivity;
+import com.google.gson.Gson;
 
 import java.io.File;
 
@@ -27,18 +29,31 @@ public class MainActivity extends AppCompatActivity {
      */
     private static final int REQUEST_CODE_GENERAL_BASIC = 100;
     /**
+     * 通用文字识别（高精度）请求码
+     */
+    private static final int REQUEST_CODE_ACCURATE_BASIC = 101;
+
+    /**
      * 对应百度平台上的应用apiKey
      */
     private String apiKey = "gQm5vnWxGuz5khN4IZ16yriL";
+
     /**
      * 对应百度平台上的应用secretKey
      */
     private String secretKey = "c8t796hbq0DXdsngSsOou5FCK2fFckpn";
 
+    /**
+     * 弹窗
+     */
+    private AlertDialog.Builder mDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //实例化，否则使用会报错 null object
+        mDialog = new AlertDialog.Builder(this);
 
         initTextSDK();
     }
@@ -51,29 +66,31 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResult(AccessToken result) {
                 String token = result.getAccessToken();
-                Log.d("result-->","成功！"+token);
+                Log.d("result-->", "成功！" + token);
             }
 
             @Override
             public void onError(OCRError error) {
                 error.printStackTrace();
-                Log.d("result-->","失败！"+error.getMessage());
+                Log.d("result-->", "失败！" + error.getMessage());
 
             }
-        }, getApplicationContext(),  apiKey, secretKey);
+        }, getApplicationContext(), apiKey, secretKey);
     }
 
     /**
      * Toast提示
+     *
      * @param msg
      */
     private void showMsg(String msg) {
-        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
 
     /**
      * 获取保存文件
+     *
      * @param context
      * @return
      */
@@ -84,6 +101,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * 通用文字识别
+     *
      * @param view
      */
     public void generalBasic(View view) {
@@ -96,10 +114,22 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, REQUEST_CODE_GENERAL_BASIC);
     }
 
+    /**
+     * 通用文字识别 （高精度版）
+     *
+     * @param view
+     */
+    public void highPrecision(View view) {
+        Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+        intent.putExtra(CameraActivity.KEY_OUTPUT_FILE_PATH, getSaveFile(getApplication()).getAbsolutePath());
+        intent.putExtra(CameraActivity.KEY_CONTENT_TYPE, CameraActivity.CONTENT_TYPE_GENERAL);
+        startActivityForResult(intent, REQUEST_CODE_ACCURATE_BASIC);
+    }
 
 
     /**
      * Activity回调
+     *
      * @param requestCode
      * @param resultCode
      * @param data
@@ -114,9 +144,48 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void onResult(String result) {
                             showMsg(result);
-                            Log.d("result-->",result);
+                            Log.d("result-->", result);
+                        }
+                    });
+        }
+        // 识别成功回调，通用文字识别（高精度版）
+        if (requestCode == REQUEST_CODE_ACCURATE_BASIC && resultCode == Activity.RESULT_OK) {
+            RecognizeService.recAccurateBasic(this, getSaveFile(getApplicationContext()).getAbsolutePath(),
+                    new RecognizeService.ServiceListener() {
+                        @Override
+                        public void onResult(String result) {
+                            //弹窗显示识别内容
+                            showDialog(result);
+                            Log.d("result-->", result);
                         }
                     });
         }
     }
+
+    /**
+     * 显示识别结果弹窗
+     * @param result
+     */
+    private void showDialog(final String result) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                //数据解析 Gson将JSON字符串转为实体Bean
+                TextResult textResult = new Gson().fromJson(result, TextResult.class);
+                if (textResult.getWords_result() == null && textResult.getWords_result().size() <= 0) {
+                    return;
+                }
+                String text = "";
+                //数据不为空并且大于0
+                for(int i = 0;i<textResult.getWords_result().size();i++){
+                    text += textResult.getWords_result().get(i).getWords()+"\n";
+                }
+                mDialog.setMessage(text)
+                        .setPositiveButton("确定", null)
+                        .show();
+            }
+        });
+    }
+
+
 }
